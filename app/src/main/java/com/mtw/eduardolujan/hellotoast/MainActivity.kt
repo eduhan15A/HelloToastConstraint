3300ecd8282a7de8b25f14c15d73ea0d2f0012db
eduardolujan <infinito15A>
2018-05-30 22:08:29 -0500

    Práctica Terminada

--- app/src/main/java/com/mtw/eduardolujan/hellotoast/MainActivity.kt
+++ app/src/main/java/com/mtw/eduardolujan/hellotoast/MainActivity.kt
@@ -2,11 +2,37 @@ package com.mtw.eduardolujan.hellotoast
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.TextView
+import android.widget.Toast
+
+
 
 class MainActivity : AppCompatActivity() {
+
+    private var mCount = 0
+    private var mShowCount: TextView? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        mShowCount =  findViewById(R.id.show_count) as TextView
+    }
+
+    fun countUp(view: View) {
+        // What happens when user clicks on the button_count Button goes here.
+        mCount = mCount +1
+        if (mShowCount != null)
+            mShowCount!!.setText(mCount.toString() );
+    }
+
+
+    fun showToast(view: View) {
+        // What happens when user clicks on the button_toast Button goes here.
+
+        val toast = Toast.makeText(applicationContext, getString(R.string.toast_message), Toast.LENGTH_LONG)
+        toast.show();
+
     }
 }
